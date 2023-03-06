6f7961a0516dfdc98b496bc67ef15561900365e7
Maciej Muter <maciejmuter@gmail.com>
2023-03-06 09:20:31 +0100

    all working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,15 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import "./App.css";
-import AddTodoForm from "./components/AddTodoForm";
-import { nanoid } from "nanoid";
-import Todo from "./types/Todo";
 import TodosList from "./components/TodosList";
 import ReactSwitch from "react-switch";
 
 export const ThemeContext = createContext("light");
 
-function App() {
-  const [todos, setTodos] = useState<Todo[]>(() =>
-    JSON.parse(localStorage.getItem("todos") || "[]")
-  );
-
+const App = () => {
   const [theme, setTheme] = useState<string>("light");
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
-  };
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const sort = () => {
-    setTodos((oldTodos) => {
-      const newArray: Todo[] = [];
-      for (let i = 0; i < oldTodos.length; i++) {
-        if (oldTodos[i].isImportant) {
-          newArray.unshift(oldTodos[i]);
-        } else {
-          newArray.push(oldTodos[i]);
-        }
-      }
-      return newArray;
-    });
-  };
-
-  const handleAdd = (newTodo: string) => {
-    if (newTodo)
-      setTodos([
-        ...todos,
-        { id: nanoid(), todo: newTodo, isDone: false, isImportant: false },
-      ]);
-  };
-
-  const handleDelete = (id: string) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  };
-
-  const handleDone = (id: string) => {
-    setTodos(todos.map((t) => (t.id === id ? { ...t, isDone: !t.isDone } : t)));
-  };
-
-  const handleImportant = (id: string) => {
-    setTodos(
-      todos.map((t) =>
-        t.id === id ? { ...t, isImportant: !t.isImportant } : t
-      )
-    );
-    sort();
   };
 
   return (
@@ -70,37 +20,10 @@ function App() {
           <p>Dark mode</p>
           <ReactSwitch onChange={toggleTheme} checked={theme === "light"} />
         </div>
-        <AddTodoForm handleAdd={handleAdd} />
-        <TodosList
-          todos={todos}
-          handleDelete={handleDelete}
-          handleDone={handleDone}
-          handleImportant={handleImportant}
-        />
+        <TodosList />
       </div>
     </ThemeContext.Provider>
   );
-}
+};
 
 export default App;
-
-// const handleAdd = (newTodo: string) => {
-//   if (newTodo)
-//   setTodos((oldTodos) => {
-//     const newArray: Todo[] = [];
-//     for (let i = 0; i < oldTodos.length; i++) {
-//       if (oldTodos[i].isImportant) {
-//         newArray.unshift(oldTodos[i]);
-//       } else {
-//         newArray.push(oldTodos[i]);
-//       }
-//     }
-//       newArray.push({
-//         id: nanoid(),
-//         todo: newTodo,
-//         isDone: false,
-//         isImportant: false,
-//       });
-//     return newArray;
-//   });
-// };
--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,28 +1,71 @@
+import { useEffect, useState } from "react";
 import Todo from "../types/Todo";
-import TodoOptions from "../types/TodoOptions";
+import AddTodoForm from "./AddTodoForm";
 import SingleTodo from "./SingleTodo";
+import { nanoid } from "nanoid";
 
-interface Props extends TodoOptions {
-  todos: Todo[];
-}
+export default function TodosList() {
+  const [todos, setTodos] = useState<Todo[]>(() =>
+    JSON.parse(localStorage.getItem("todos") || "[]")
+  );
 
-export default function TodosList({
-  todos,
-  handleDelete,
-  handleDone,
-  handleImportant,
-}: Props) {
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleAdd = (newTodo: string) => {
+    if (newTodo)
+      setTodos([
+        ...todos,
+        { id: nanoid(), todo: newTodo, isDone: false, isImportant: false },
+      ]);
+  };
+
+  const sortTodos = () => {
+    setTodos((oldTodos) => {
+      const newArray: Todo[] = [];
+      for (let i = 0; i < oldTodos.length; i++) {
+        if (oldTodos[i].isImportant) {
+          newArray.unshift(oldTodos[i]);
+        } else {
+          newArray.push(oldTodos[i]);
+        }
+      }
+      return newArray;
+    });
+  };
+
+  const handleDelete = (id: string) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
+
+  const handleDone = (id: string) => {
+    setTodos(todos.map((t) => (t.id === id ? { ...t, isDone: !t.isDone } : t)));
+  };
+
+  const handleImportant = (id: string) => {
+    setTodos(
+      todos.map((t) =>
+        t.id === id ? { ...t, isImportant: !t.isImportant } : t
+      )
+    );
+    sortTodos();
+  };
+
   return (
-    <div className="todos__container">
-      {todos.map((t) => (
-        <SingleTodo
-          todo={t}
-          key={t.id}
-          handleDelete={handleDelete}
-          handleDone={handleDone}
-          handleImportant={handleImportant}
-        />
-      ))}
-    </div>
+    <>
+      <AddTodoForm handleAdd={handleAdd} />
+      <div className="todos__container">
+        {todos.map((t) => (
+          <SingleTodo
+            todo={t}
+            key={t.id}
+            handleDelete={handleDelete}
+            handleDone={handleDone}
+            handleImportant={handleImportant}
+          />
+        ))}
+      </div>
+    </>
   );
 }
